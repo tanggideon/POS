8f4a3bf69c30bb4c1ecddab30390a24e65208c22
Gideon Tang <gideon@Gideon.gideon.com>
2025-11-05 23:55:49 +0000

    Another POS edit

---
+++ app/(pos)/(components)/PosTable.tsx
@@ -0,0 +1,88 @@
+import { Input } from "@/components/ui/input"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Club Beer 750ml",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Aristocrat Premium WHiskey 500ml",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Cardinal Strawberry 1L",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
+export function PosTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">s/n</TableHead>
+          <TableHead>Item</TableHead>
+          <TableHead>Quantity</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+
+        {invoices.map((invoice, index) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{index + 1}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell><Input type="number" defaultValue={1} className="w-50" min={1}/></TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3} className="font-bold">Total</TableCell>
+          <TableCell className="text-right font-bold text-gray-700 text-lg">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  )
+}
--- app/(pos)/pos/page.tsx
+++ app/(pos)/pos/page.tsx
@@ -1,6 +1,8 @@
 import { CompanyDetails } from "@/lib/constants";
+import { Separator } from "@radix-ui/react-separator";
 import { IconLocation, IconMail, IconPhoneCall } from "@tabler/icons-react";
 import React from "react";
+import { PosTable } from "../(components)/PosTable";
 
 const Details = CompanyDetails;
 
@@ -8,7 +10,7 @@ const POS = () => {
   return (
     <div className="flex flex-1 w-full min-h-screen">
       <div className="grid grid-cols-5 grid-rows-5 gap-5 px-10 mt-10 w-full">
-        <div className="w-full flex-1 bg-white py-5 rounded-md border-2 border-amber-500 col-span-3 row-span-4">
+        <div className="w-full flex-1 bg-white py-5 rounded-md border-2 px-10 border-amber-500 col-span-3 row-span-4">
           <div className="flex flex-col items-center justify-center gap-3">
             <h1 className="font-bold text-gray-700 text-2xl">{Details.name}</h1>
             <div className="flex items-center gap-5">
@@ -31,6 +33,18 @@ const POS = () => {
                 </span>
               </div>
             </div>
+          </div>
+          <Separator className="my-5 h-1 bg-gray-300 w-full" />
+          <div>
+            {/* input field with filtering options to search for products by name or code */}
+            <input
+              type="text"
+              autoFocus
+              placeholder="Search for products by name or code..."
+              className="w-full border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+            />
+            <PosTable />
+            <Separator className="my-5 h-1 bg-gray-300 w-full" />
           </div>
         </div>
         <div className="w-full bg-gray-500"></div>
